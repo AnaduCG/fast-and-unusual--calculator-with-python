ff7ac667a028edba9b1acccdb1fb51960adc3f7e
AnaduCG <godzionwin@gmail.com>
2023-01-01 12:32:35 +0100

    corrected

--- calculator.py
+++ calculator.py
@@ -6,19 +6,19 @@ try:
         str = input('calculate: ')
         if '+' in str:
             out_put = str.split('+')
-            print(int(out_put[0]) + int(out_put[1]))
+            print(f"Ans: {int(out_put[0]) + int(out_put[1])}")
         elif '-' in str:
             out_put = str.split('-')
-            print(int(out_put[0]) - int(out_put[1]))
+            print(f"Ans: {int(out_put[0]) - int(out_put[1])}")
         elif '*' in str:
             out_put = str.split('*')
-            print(int(out_put[0]) * int(out_put[1]))
+            print(f"Ans: {int(out_put[0]) * int(out_put[1])}")
         elif '/' in str:
             out_put = str.split('/')
-            print(int(out_put[0]) / int(out_put[1]))
+            print(f"Ans: {int(out_put[0]) / int(out_put[1])}")
         elif 'end' in str:
             break
         else:
-            print(f"ans: 0")
+            print(f"Ans: 0")
 except (ValueError) as e:
     print(f'Error: {e}')
